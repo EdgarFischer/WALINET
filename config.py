91d5b51765f22e981d84f802b70abff09af9dcab
Hauke Fischer <haukee.fischer@gmail.com>
2025-05-22 09:38:31 +0000

    Neues Model, vergleich zu Pauls Netzwerk über MetabMaps

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 params ={}
 
-params["model_name"] =  "Vienna_5DataSets_1.1_3Layers"#"EXP_1"# "test"# 
+params["model_name"] =  "Vienna_5DataSets_1.1_7Layers"#"EXP_1"# "test"# 
 params["path_to_model"] = "models/" + params["model_name"] + "/"
 params["path_to_data"] = "data/"
 
@@ -13,18 +13,18 @@ params["gpu"]=1
 params["batch_size"] = 1000#32
 params["num_worker"] = 15
 params["lr"] = 0.00025 # 0.0001#0.0001 
-params["epochs"]=2000
+params["epochs"]=1000
 params["verbose"] = False#params["model_name"] == "test" #True #True #False #True #False #TrueFalse#
 params["n_batches"] = -1
 params["n_val_batches"] = -1
 params["data_version"] = 'v_1.0'#'v3_2'
 
 # LR Scheduler
-params["milestones"] = [400, 700, 1000, 1300, 1500]
+params["milestones"] = [300, 500, 700]
 params["gamma"] = 0.25
 
 # Model Params
-params["nLayers"] = 3#5#4
+params["nLayers"] = 7#5#4
 params["nFilters"] = 8#12#8#8#4#8#16#8
 params["in_channels"] = 2 
 params["out_channels"] = 2
---
+++ models/Vienna_5DataSets_1.1_7Layers/src 2025-05-21 13:19:13.780256/config.py
@@ -0,0 +1,38 @@
+params ={}
+
+params["model_name"] =  "Vienna_5DataSets_1.1_7Layers"#"EXP_1"# "test"# 
+params["path_to_model"] = "models/" + params["model_name"] + "/"
+params["path_to_data"] = "data/"
+
+params["train_subjects"]=['Vol6','Vol7','Vol8','Vol9'] #
+
+params["val_subjects"]=['Vol5']
+
+# Train Params
+params["gpu"]=1
+params["batch_size"] = 1000#32
+params["num_worker"] = 15
+params["lr"] = 0.00025 # 0.0001#0.0001 
+params["epochs"]=1000
+params["verbose"] = False#params["model_name"] == "test" #True #True #False #True #False #TrueFalse#
+params["n_batches"] = -1
+params["n_val_batches"] = -1
+params["data_version"] = 'v_1.0'#'v3_2'
+
+# LR Scheduler
+params["milestones"] = [300, 500, 700]
+params["gamma"] = 0.25
+
+# Model Params
+params["nLayers"] = 7#5#4
+params["nFilters"] = 8#12#8#8#4#8#16#8
+params["in_channels"] = 2 
+params["out_channels"] = 2
+params["dropout"] = 0.0#.005#.01# 0.005
+
+params["clean_model"] = True # Only removes models called "test"
+params["train"] = True
+params["predict"] = False
+
+params['preload'] = False
+params['preload_model'] = ''
