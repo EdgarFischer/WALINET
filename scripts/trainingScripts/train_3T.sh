0e3e80843039358fe319e886e3f75fd1f2890ca5
Hauke Fischer <haukee.fischer@gmail.com>
2026-07-23 11:22:55 +0200

    3T trainings scritps paths fixed

--- scripts/trainingScripts/train_3T.sh
+++ scripts/trainingScripts/train_3T.sh
@@ -9,7 +9,7 @@ timestamp=$(date +"%Y%m%d_%H%M%S")
 log_file="logs/logs_training/train_${timestamp}.log"
 
 nohup env PYTHONPATH=src python3 -u scripts/trainingScripts/train.py \
-  --config ../configs/train_3T.yaml \
+  --config configs/Training/train_3T.yaml \
   > "$log_file" 2>&1 &
 
 echo "Started training."
